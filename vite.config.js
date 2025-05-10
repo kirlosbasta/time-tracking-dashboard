0bda1f292056b7ac8a791a65660706a4d820ee93
Kirlos Basta <kirlosbasta@gmail.com>
2025-05-10 18:49:03 +0300

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://kirlosbasta.github.io/time-tracking-dashboard/',
+  base: '/time-tracking-dashboard/',
   plugins: [react(), tailwindcss()],
 });
 
